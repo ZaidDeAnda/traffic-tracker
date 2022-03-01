7361c585da6fd3eea6aab57e0ca8428374558777
ZaidDeAnda <zaidthejedi@gmail.com>
2022-03-01 17:03:58 -0600

    añadido yolov5

---
+++ main.py
@@ -0,0 +1,171 @@
+from centroid_tracker import CentroidTracker
+from trackable_object import TrackableObject
+from yolov5.yolov5 import YoloV5
+from yolov5.utils.image_loader import ImageLoader
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+def main_func():
+    output_path = "Output.mp4"
+
+    print("[INFO] starting video stream...")
+    vs = cv2.VideoCapture("/home/zaid/github/traffic-tracker/petal_20220217_160339.mp4")
+    time.sleep(2.0)
+
+    # initialize the video writer (we'll instantiate later if need be)
+    writer = None
+    # initialize the frame dimensions (we'll set them as soon as we read
+    # the first frame from the video)
+    W = None
+    H = None
+    # instantiate our centroid tracker, then initialize a list to store
+    # each of our dlib correlation trackers, followed by a dictionary to
+    # map each unique object ID to a TrackableObject
+    ct = CentroidTracker(maxDisappeared=40)
+    trackableObjects = {}
+    # initialize the total number of frames processed thus far, along
+    # with the total number of objects that have moved either up or down
+    totalFrames = 0
+    totalDown = 0
+    totalUp = 0
+    # start the frames per second throughput estimator
+    fps = FPS().start()
+
+    # loop over frames from the video stream
+    while True:
+        # grab the next frame and handle if we are reading from either
+        # VideoCapture or VideoStream
+        frame = vs.read()
+        frame = frame[1]
+        # if we are viewing a video and we did not grab a frame then we
+        # have reached the end of the video
+        # resize the frame to have a maximum width of 500 pixels (the
+        # less data we have, the faster we can process it), then convert
+        # the frame from BGR to RGB for dlib
+        frame = imutils.resize(frame, width=500)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # if the frame dimensions are empty, set them
+        if W is None or H is None:
+            (H, W) = frame.shape[:2]
+        # if we are supposed to be writing a video to disk, initialize
+        # the writer
+        if writer is None:
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(output_path, fourcc, 30,
+                (W, H), True)
+        # initialize the current status along with our list of bounding
+        # box rectangles returned by either (1) our object detector or
+        # (2) the correlation trackers
+        status = "Waiting"
+        rects = []
+        # check to see if we should run a more computationally expensive
+        # object detection method to aid our tracker
+        status = "Detecting"
+        im, im0s = ImageLoader.PreprocessImage(rgb)
+        predictions = YoloV5.get_bounding_boxes(im, im0s)
+
+        for prediction in predictions:
+            x_start, y_start, x_end, y_end = predictions[prediction]["bounding_box"].values()
+            rects.append([x_start, y_start, x_end, y_end])
+        print(f"se encontraron {len(rects)} objetos")
+
+        # draw a horizontal line in the center of the frame -- once an
+        # object crosses this line we will determine whether they were
+        # moving 'up' or 'down'
+        cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
+        # use the centroid tracker to associate the (1) old object
+        # centroids with (2) the newly computed object centroids
+        objects = ct.update(rects)
+        # loop over the tracked objects
+        for (objectID, centroid) in objects.items():
+            # check to see if a trackable object exists for the current
+            # object ID
+            to = trackableObjects.get(objectID, None)
+            # if there is no existing trackable object, create one
+            if to is None:
+                # print(f"nuevo registro {objectID}")
+                to = TrackableObject(objectID, centroid)
+            # otherwise, there is a trackable object so we can utilize it
+            # to determine direction
+            else:
+                # print(f"Continuando registro de {objectID}")
+                # the difference between the y-coordinate of the *current*
+                # centroid and the mean of *previous* centroids will tell
+                # us in which direction the object is moving (negative for
+                # 'up' and positive for 'down')
+                y = [c[1] for c in to.centroids]
+                direction = centroid[1] - np.mean(y)
+                to.centroids.append(centroid)
+                # check to see if the object has been counted or not
+                if not to.counted:
+                    # if the direction is negative (indicating the object
+                    # is moving up) AND the centroid is above the center
+                    # line, count the object
+                    if direction < 0 and centroid[1] < H // 2:
+                        totalUp += 1
+                        to.counted = True
+                    # if the direction is positive (indicating the object
+                    # is moving down) AND the centroid is below the
+                    # center line, count the object
+                    elif direction > 0 and centroid[1] > H // 2:
+                        totalDown += 1
+                        to.counted = True
+            # store the trackable object in our dictionary
+            trackableObjects[objectID] = to
+            # draw both the ID of the object and the centroid of the
+            # object on the output frame
+            text = "ID {}".format(objectID)
+            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+            print("Objeto dibujado")
+        # construct a tuple of information we will be displaying on the
+        # frame
+        info = [
+            ("Up", totalUp),
+            ("Down", totalDown),
+            ("Status", status),
+        ]
+        # loop over the info tuples and draw them on our frame
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        # check to see if we should write the frame to disk
+        if writer is not None:
+            writer.write(frame)
+        # # show the output frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+        # increment the total number of frames processed thus far and
+        # then update the FPS counter
+        totalFrames += 1
+        print(f"frame #{totalFrames}")
+        fps.update()
+        # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # check to see if we need to release the video writer pointer
+    if writer is not None:
+        writer.release()
+    # if we are not using a video file, stop the camera video stream
+    vs.stop()
+    # close any open windows
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main_func()
