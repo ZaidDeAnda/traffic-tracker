2b65814020bb52c9f13af8c2360a9a66780b678f
ZaidDeAnda <zaidthejedi@gmail.com>
2022-03-02 00:15:20 -0600

    sort añadido

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import imutils
 import time
 import dlib
 import cv2
+from sort import *
 
 import warnings
 
@@ -31,7 +32,7 @@ def main_func():
     # instantiate our centroid tracker, then initialize a list to store
     # each of our dlib correlation trackers, followed by a dictionary to
     # map each unique object ID to a TrackableObject
-    ct = CentroidTracker(maxDisappeared=40)
+    mot_tracker = Sort(max_age=30, min_hits=3, iou_threshold=0.5)
     trackableObjects = {}
     # initialize the total number of frames processed thus far, along
     # with the total number of objects that have moved either up or down
@@ -67,7 +68,7 @@ def main_func():
         # box rectangles returned by either (1) our object detector or
         # (2) the correlation trackers
         status = "Waiting"
-        rects = []
+        rects = np.zeros([5])
         # check to see if we should run a more computationally expensive
         # object detection method to aid our tracker
         status = "Detecting"
@@ -77,69 +78,59 @@ def main_func():
         for prediction in predictions:
             if predictions[prediction]["confidence"] > 0.5:
                 x_start, y_start, x_end, y_end = predictions[prediction]["bounding_box"].values()
-                rects.append([x_start, y_start, x_end, y_end])
-
+                array = np.array([x_start, y_start, x_end, y_end, predictions[prediction]["confidence"]])
+                rects = np.vstack((rects, array))
+        rects = np.delete(rects, 0, 0)
+        print(rects)
         # draw a horizontal line in the center of the frame -- once an
         # object crosses this line we will determine whether they were
         # moving 'up' or 'down'
         cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
         # use the centroid tracker to associate the (1) old object
         # centroids with (2) the newly computed object centroids
-        objects = ct.update(rects)
+        objects = mot_tracker.update(rects)
         # loop over the tracked objects
-        for (objectID, centroid) in objects.items():
-            # check to see if a trackable object exists for the current
-            # object ID
-            to = trackableObjects.get(objectID, None)
-            # if there is no existing trackable object, create one
-            if to is None:
-                # print(f"nuevo registro {objectID}")
-                to = TrackableObject(objectID, centroid)
-            # otherwise, there is a trackable object so we can utilize it
-            # to determine direction
-            else:
-                # print(f"Continuando registro de {objectID}")
-                # the difference between the y-coordinate of the *current*
-                # centroid and the mean of *previous* centroids will tell
-                # us in which direction the object is moving (negative for
-                # 'up' and positive for 'down')
-                y = [c[1] for c in to.centroids]
-                direction = centroid[1] - np.mean(y)
-                to.centroids.append(centroid)
-                # check to see if the object has been counted or not
-                if not to.counted:
-                    # if the direction is negative (indicating the object
-                    # is moving up) AND the centroid is above the center
-                    # line, count the object
-                    if direction < 0 and centroid[1] < H // 2:
-                        totalUp += 1
-                        to.counted = True
-                    # if the direction is positive (indicating the object
-                    # is moving down) AND the centroid is below the
-                    # center line, count the object
-                    elif direction > 0 and centroid[1] > H // 2:
-                        totalDown += 1
-                        to.counted = True
-            # store the trackable object in our dictionary
-            trackableObjects[objectID] = to
-            # draw both the ID of the object and the centroid of the
-            # object on the output frame
-            text = "ID {}".format(objectID)
-            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-        # construct a tuple of information we will be displaying on the
-        # # frame
-        # info = [
-        #     ("Up", totalUp),
-        #     ("Down", totalDown),
-        #     ("Status", status),
-        # ]
-        # # loop over the info tuples and draw them on our frame
-        # for (i, (k, v)) in enumerate(info):
-        #     text = "{}: {}".format(k, v)
-        #     cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-        #         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        # for (objectID, centroid) in objects.items():
+        #     # check to see if a trackable object exists for the current
+        #     # object ID
+        #     to = trackableObjects.get(objectID, None)
+        #     # if there is no existing trackable object, create one
+        #     if to is None:
+        #         # print(f"nuevo registro {objectID}")
+        #         to = TrackableObject(objectID, centroid)
+        #     # otherwise, there is a trackable object so we can utilize it
+        #     # to determine direction
+        #     else:
+        #         # print(f"Continuando registro de {objectID}")
+        #         # the difference between the y-coordinate of the *current*
+        #         # centroid and the mean of *previous* centroids will tell
+        #         # us in which direction the object is moving (negative for
+        #         # 'up' and positive for 'down')
+        #         y = [c[1] for c in to.centroids]
+        #         direction = centroid[1] - np.mean(y)
+        #         to.centroids.append(centroid)
+        #         # check to see if the object has been counted or not
+        #         if not to.counted:
+        #             # if the direction is negative (indicating the object
+        #             # is moving up) AND the centroid is above the center
+        #             # line, count the object
+        #             if direction < 0 and centroid[1] < H // 2:
+        #                 totalUp += 1
+        #                 to.counted = True
+        #             # if the direction is positive (indicating the object
+        #             # is moving down) AND the centroid is below the
+        #             # center line, count the object
+        #             elif direction > 0 and centroid[1] > H // 2:
+        #                 totalDown += 1
+        #                 to.counted = True
+        #     # store the trackable object in our dictionary
+        #     trackableObjects[objectID] = to
+        #     # draw both the ID of the object and the centroid of the
+        #     # object on the output frame
+        #     text = "ID {}".format(objectID)
+        #     cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+        #         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        #     cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
         # check to see if we should write the frame to disk
         if writer is not None:
             writer.write(frame)
